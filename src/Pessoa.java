da3be3cbe7d4bad518737e46d68188195c5e6c63
João_A.11 <jarlpereira29@gmail.com>
2025-02-07 14:59:31 -0300

    Adiciona classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+    private DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getDataNascimentoFormatada() {
+        return this.dataNascimento.format(formato);
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
