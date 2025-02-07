a465a48d3587deb6283d928c9315dd5571212f2f
João_A.11 <jarlpereira29@gmail.com>
2025-02-07 15:46:28 -0300

    Adiciona classe Funcionario

---
+++ src/Funcionario.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class Funcionario extends Pessoa {
+
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public String getSalarioFormatado() {
+        return String.format(Locale.GERMAN,"%,.2f", this.salario);
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
