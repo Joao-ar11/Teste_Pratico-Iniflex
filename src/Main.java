b15e21506a0b80c8768f285ec16576c2e545241e
João_A.11 <jarlpereira29@gmail.com>
2025-02-07 16:16:15 -0300

    Adiciona getListFuncionarios a Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World!");
+        
+    }
+
+    private static ArrayList<Funcionario> getListaFuncionarios() {
+        ArrayList<Funcionario> lista = new ArrayList<Funcionario>();
+
+        lista.add(new Funcionario("Maria",
+                LocalDate.of(2000, 10, 18),
+                new BigDecimal(2009.44),
+                "Operador"
+        ));
+
+        lista.add(new Funcionario("João",
+                LocalDate.of(1990, 5, 12),
+                new BigDecimal(2284.38),
+                "Operador"
+        ));
+
+        lista.add(new Funcionario("Caio",
+                LocalDate.of(1961, 5, 2),
+                new BigDecimal(9836.14),
+                "Coordenador"
+        ));
+
+        lista.add(new Funcionario("Miguel",
+                LocalDate.of(1988, 10, 14),
+                new BigDecimal(19119.88),
+                "Diretor"
+        ));
+
+        lista.add(new Funcionario("Alice",
+                LocalDate.of(1995, 1, 5),
+                new BigDecimal(2234.68),
+                "Recepcionista"
+        ));
+
+        lista.add(new Funcionario("Heitor",
+                LocalDate.of(1999, 11, 19),
+                new BigDecimal(1582.72),
+                "Operador"
+        ));
+
+        lista.add(new Funcionario("Arthur",
+                LocalDate.of(1993, 3, 31),
+                new BigDecimal(4071.84),
+                "Contador"
+        ));
+
+        lista.add(new Funcionario("Laura",
+                LocalDate.of(1994, 7, 8),
+                new BigDecimal(3017.45),
+                "Gerente"
+        ));
+
+        lista.add(new Funcionario("Heloísa",
+                LocalDate.of(2003, 5, 24),
+                new BigDecimal(1606.85),
+                "Eletricista"
+        ));
+
+        lista.add(new Funcionario("Helena",
+                LocalDate.of(1996, 9, 2),
+                new BigDecimal(2799.93),
+                "Gerente"
+        ));
+
+        return lista;
     }
 }
